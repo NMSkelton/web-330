d78adb285632e7c1920a3ae74ffcd2961a6d9b20
Nicholas Skelton <nicholasskelton30@gmail.com>
2025-11-20 23:15:25 -0600

    Week 5 Push

--- week-5/restaurant-reservation/script.js
+++ week-5/restaurant-reservation/script.js
@@ -3,24 +3,47 @@
   Chapter 2
   Programming Assignment
 
-  Author:
-  Date:
-  Filename:
+  Author: Nicholas Skelton
+  Date: 11.20.25
+  Filename: script.js
 */
 
 // Create an in-memory object array for each table in the restaurant
 let tables = [
-  // Add your table objects here
+  { tableNumber: 1, capacity: 6, isReserved: true },
+  { tableNumber: 2, capacity: 8, isReserved: true },
+  { tableNumber: 3, capacity: 10, isReserved: false },
 ];
 
 // Create a function reserveTable
 function reserveTable(tableNumber, callback, time) {
-  // Add your code here
+  for (let t of tables) {
+    if (t.tableNumber  === tableNumber) {
+      if (t.isReserved) {  // If table is resevered
+        document.getElementById("message").textContent = "Table is already reserved.";
+        callback("Table is already reserved.");
+      } else {  // If table is not reserved
+        setTimeout(() => {
+          t.isReserved = true;  // Set reserve status to true
+          document.getElementById("message").textContent = "Reservation succesful!";
+          callback("Reservation successful!");
+        }, time);
+      }
+      break;
+    }
+  }
 }
+
 
 // When the form is submitted, call the reserveTable function
 document
   .getElementById("reservationForm")
   .addEventListener("submit", function (e) {
-    // Add your code here
-  });
+    e.preventDefault();  // Prevent submission, so message sticks
+
+    const tableNumber = parseInt(document.getElementById("tableNumber").value);  // Pull tableNumber and convert it to an integer
+
+    reserveTable(tableNumber, function(message) {
+      document.getElementById("message").textContent = message;
+    }, 1000);
+});
