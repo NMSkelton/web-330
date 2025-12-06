31d02cfbd32168ea3bd1a36ee15ee19a4f744c03
Nicholas Skelton <nicholasskelton30@gmail.com>
2025-12-05 18:58:22 -0600

    Week 7 Push

--- week-7/chef-dashboard/script.js
+++ week-7/chef-dashboard/script.js
@@ -3,31 +3,80 @@
   Chapter 3
   Programming Assignment
 
-  Author:
-  Date:
+  Author: Nicholas Skelton
+  Date: 12.5.25
   Filename: chefs.js
 */
 
 "use strict";
 
-// TODO: Define an array of chef objects
 let chefs = [
-  // Each chef object should have a name, specialty, weakness, and restaurantLocation
+  { name: "Gus \"The Bus\" Johnson", specialty: "desserts", weakness: "salads", restaurantLocation: "New York City" },
+  { name: "John \"This Guy Surely Won't Make It\" Doe", specialty: "proteins", weakness: "appetizers", restaurantLocation: "Miami" },
+  { name: "Betty \"Not Crocker\" White", specialty: "cocktails", weakness: "fish", restaurantLocation: "London" },
 ];
 
-// TODO: Define a function to retrieve the first chef's information
 function retrieveChef1() {
-  // This function should return a promise that resolves with the chef's information after a delay
-}
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(chefs[0]);
+    }, 2000);
+  });
+};
 
-// TODO: Define a function to retrieve the second chef's information
+// Chef rejected manually
 function retrieveChef2() {
-  // This function should return a promise that resolves with the chef's information after a delay
-}
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject("Chef 2 could not be found");
+    }, 3000);
+  });
+};
 
-// TODO: Define a function to retrieve the third chef's information
+
 function retrieveChef3() {
-  // This function should return a promise that resolves with the chef's information after a delay
-}
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(chefs[2]);
+    }, 4000);
+  });
+};
 
-// TODO: Use Promise.allSettled to retrieve all chefs' information and update the webpage accordingly
+
+Promise.allSettled([retrieveChef1(), retrieveChef2(), retrieveChef3()]).then((results) => {
+  if (results[0].status === "fulfilled") {
+    document.getElementById("chef1").innerHTML =
+    `<h1>${results[0].value.name}</h1>
+    <ul>
+      <li>Specialty: ${results[0].value.specialty}</li>
+      <li>Weakness: ${results[0].value.weakness}</li>
+      <li>Location: ${results[0].value.restaurantLocation}</li>
+    </ul>`;
+  } else {
+    document.getElementById("chef1").innerHTML = `<h1>Oh no! This chef must have been terminated.</h1>`;
+  }
+
+  if (results[1].status === "fulfilled") {
+    document.getElementById("chef2").innerHTML =
+    `<h1>${results[1].value.name}</h1>
+    <ul>
+      <li>Specialty: ${results[1].value.specialty}</li>
+      <li>Weakness: ${results[1].value.weakness}</li>
+      <li>Location: ${results[1].value.restaurantLocation}</li>
+    </ul>`;
+  } else {
+    document.getElementById("chef2").innerHTML = `<h1>Oh no! This chef must have been terminated.</h1>`;
+  }
+
+  if (results[2].status === "fulfilled") {
+    document.getElementById("chef3").innerHTML =
+    `<h1>${results[2].value.name}</h1>
+    <ul>
+      <li>Specialty: ${results[2].value.specialty}</li>
+      <li>Weakness: ${results[2].value.weakness}</li>
+      <li>Location: ${results[2].value.restaurantLocation}</li>
+    </ul>`;
+  } else {
+    document.getElementById("chef3").innerHTML = `<h1>Oh no! This chef must have been terminated.</h1>`;
+  }
+});
